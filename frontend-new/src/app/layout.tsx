a1f06bfccca5e4daaf0961986d99a64e584b1e8e
Hari Mansharamani <mansharamanihari@gmail.com>
2025-02-09 17:39:41 -0600

    changed yaml

--- frontend-new/src/app/layout.tsx
+++ frontend-new/src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={geist.className}>
+      <body>
         <Providers>
           {children}
         </Providers>
